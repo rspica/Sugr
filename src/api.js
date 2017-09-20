bcd3e1b46f0843e75e98a3b98a40c16f9f374a35
Jane Lee <janelee@Janes-MacBook-Pro-2.local>
2017-09-19 20:32:01 -0400

    funda api code

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
 const BASEURL = 'https://api.nutritionix.com/v1_1/search/';
 const APIKEY = '5234f7f1&appKey=40a5d6ab8411eb1e9d9f23f601944842';
+
 
 export default {
 	search: function(query) {
