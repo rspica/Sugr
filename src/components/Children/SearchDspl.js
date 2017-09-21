58eb70c80aff08337fa6db2c6a2c78ee517b100a
Admin <Admin@Eliss-MacBook-Air.local>
2017-09-21 18:45:40 -0400

    rendering api results with transition display

--- src/components/Children/SearchDspl.js
+++ src/components/Children/SearchDspl.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 
-import SearchResult from './SearchResult';
+
+const AnimStyle = {
+    transition: 'top 2.5s ease-out',
+  };
 
 export default class SearchDspl extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
     render() {
         console.log('prpos indise of searchDspl: ', this.props)
         return (
-            <div className="resultWell">
-                <div>
-                    <h1>search results</h1>
+            <div className="resultWell" 
+                style = {{ ...AnimStyle, top: this.props.top }}>
+                <div className="resutTitle">
+                    <h1>Search Results</h1>
                 </div>
-
+                
                 <div>
                 { this.props.foodType ? (
                     <ul>
                         { this.props.foodType.map(foodItem => {
                             var { item_name, brand_name, nf_sugar, nf_serving_size_qty } = foodItem.fields
                                 return  <li key={foodItem._id}>
-                                        <h2> { foodItem.fields.item_name } { foodItem.fields.brand_name }</h2>
-                                        <h2>serving size: { foodItem.fields.nf_serving_size_qty }</h2>
-                                        <h2>sugars: { foodItem.fields.nf_sugars } <span>g</span></h2>
+                                            <h2 className="foodItem"><strong>{ foodItem.fields.brand_name }</strong> { foodItem.fields.item_name }</h2>
+                                            <div className="foodStat">
+                                                <h2>serving size: { foodItem.fields.nf_serving_size_qty }  {  }
+                                                  |  sugars: { foodItem.fields.nf_sugars } <span>g</span></h2>
+                                            </div>
                                         </li>
                         }
                         )}
@@ -28,7 +38,10 @@ export default class SearchDspl extends Component {
                 ) : (
                     <h3>No Results to Display</h3>
                     )}
-                </div>     
+                </div>  
+                <div>
+                    
+                </div> 
             </div>
         )
     }
