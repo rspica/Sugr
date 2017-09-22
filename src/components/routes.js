3932c67d614c46924b07dc4db514c7eb7064f8d4
funduval <fundaduval@gmail.com>
2017-09-22 00:31:19 -0400

    cleaning up routes but still issues

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Dashboard from '../dashboard'
-import App from '../App'
-import { Router, Route, IndexRoute, Link, browserHistory} from 'react-router'
+import React from 'react';
+import { render } from 'react-dom';
+import Dashboard from '../dashboard';
+import App from '../App';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
 const Routes = () => (
 	//Setting up routes
-		<Router history={browserHistory}>
-			<Route path="/" component={App}></Route>
-			<Route path="/dashboard" component={Dashboard}></Route>
-		</Router>
-)
+	<Router history={browserHistory}>
+		<Route exact path="/" component={App} />
+		<Route exact path="/dashboard" component={Dashboard} />
+	</Router>
+);
 
 export default Routes;
